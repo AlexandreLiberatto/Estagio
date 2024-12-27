ef95297e429a4b60172236c5ee098d3402e02bca
Alexandre liberato <alexandreliberatto@gmail.com>
2024-12-27 08:10:23 -0300

    Deletando arquivo conceitos e criando class Fibonacci
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

---
+++ alexandre/src/main/java/com/Fibonacci.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Fibonacci {
+
+    private int numero;
+
+    //Construtor
+    public Fibonacci(int numero){
+        this.numero = numero;
+    }
+
+    //Getters e Setters
+    public int getNumero(){
+        return numero;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+}
--- alexandre/src/main/java/com/estagio/Main.java
+++
@@ -1,7 +0,0 @@
-package com.estagio;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
