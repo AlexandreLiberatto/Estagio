af960f6b9f55c9a5929c6e38c162a35b7ce7995f
Alexandre liberato <alexandreliberatto@gmail.com>
2024-12-27 17:14:48 -0300

    Criando métodos fibonacciRecursivo e fibonacciLinear
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- alexandre/src/main/java/com/Fibonacci.java
+++ alexandre/src/main/java/com/Fibonacci.java
@@ -9,6 +9,45 @@ public class Fibonacci {
         this.numero = numero;
     }
 
+    //Metodo recursivo
+    public int fibonacciRecursivo(int numero){
+        if(numero < 0){
+            throw new RuntimeException("O número precisa ser maior oum igual à zero!");
+        } 
+
+        if(numero == 0){
+            return 0;
+        } else if(numero == 1){
+            return 1;
+        }
+
+        return fibonacciRecursivo(numero - 1) + fibonacciRecursivo(numero - 2);
+    }
+
+    //Metodo linear
+    public int fibonacciLinear(int numero){
+        if(numero < 0){
+            throw new RuntimeException("O número precisa ser maior oum igual à zero!");
+        } 
+
+        if(numero == 0){
+            return 0;
+        } else if(numero == 1){
+            return 1;
+        }
+
+        int numeroAnterior = 1;
+        int numeroAnteAnterior = 0;
+
+        for(int i = 2; i <= numero; i++){
+            int totalDoisNumAnterior = numeroAnteAnterior + numeroAnterior;
+
+            numeroAnteAnterior = numeroAnterior;
+            numeroAnterior = totalDoisNumAnterior;
+        }
+        return numeroAnterior;
+    }
+
     //Getters e Setters
     public int getNumero(){
         return numero;
