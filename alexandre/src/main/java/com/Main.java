9f262f4d8d66bbc589014181b3fce051164afa37
Alexandre liberato <alexandreliberatto@gmail.com>
2024-12-27 18:29:26 -0300

    Criando class Main e testando métodos Fibonacci
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

---
+++ alexandre/src/main/java/com/Main.java
@@ -0,0 +1,42 @@
+package com;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Entrada pelo console
+        Scanner sc = new Scanner(System.in);
+
+        //Instanciando objeto
+        Fibonacci fibonacci = new Fibonacci(0);
+
+        try {
+
+        System.out.println("Digite um número para ver seu Fibonacci usando método recursivo: ");
+        int entrada1 = sc.nextInt();
+        fibonacci.setNumero(entrada1);
+        int numero = fibonacci.getNumero();
+
+        //Imprimindo Fibonacci usando método recursivo
+        fibonacci.fibonacciRecursivo(numero);
+
+        System.out.println("Digite um número para ver seu Fibonacci usando método recursivo: ");
+        int entrada2 = sc.nextInt();
+        fibonacci.setNumero(entrada2);
+        numero = fibonacci.getNumero();
+
+        //Imprimindo Fibonacci usando método linear
+        fibonacci.imprimirLinear(numero);
+            
+        } catch (Exception e) {
+            System.out.println("Ops! Aconteceu o seguinte erro: "+ e.getMessage());
+        } finally {
+            sc.close();
+        }
+
+        
+    }
+    
+}
